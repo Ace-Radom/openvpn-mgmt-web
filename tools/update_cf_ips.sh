f5de1066ccf544ca00017edb3ab18259d4eda213
Ace_Radom <sichenradomlyu@gmail.com>
2025-10-01 21:00:20 +0200

    fix: spilt ipv4 & v6

--- tools/update_cf_ips.sh
+++ tools/update_cf_ips.sh
@@ -7,9 +7,15 @@ curl -s https://www.cloudflare.com/ips-v6 -o /tmp/cf_ipv6.txt
 
 {
     echo "# Cloudflare IPs - Auto generated"
-    for ip in $(cat /tmp/cf_ipv4.txt /tmp/cf_ipv6.txt); do
-        echo "set_real_ip_from $ip;"
-    done
+
+    while read ip; do
+        [ -n "$ip" ] && echo "set_real_ip_from $ip;"
+    done < /tmp/cf_ipv4.txt
+
+    while read ip; do
+        [ -n "$ip" ] && echo "set_real_ip_from $ip;"
+    done < /tmp/cf_ipv6.txt
+
 } > "$CONF_FILE"
 
 nginx -t && systemctl reload nginx.service
