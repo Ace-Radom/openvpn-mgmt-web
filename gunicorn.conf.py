bb4be36a958dd20f24b6acdea75360e7d7ea0d85
Ace_Radom <sichenradomlyu@gmail.com>
2025-10-01 21:16:30 +0200

    update gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@ workers = 2
 forwarded_allow_ips = "*"
 
 accesslog = "-"
-access_log_format = '%({X-Forwarded-For}i)s:%({X-Client-Port}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s'
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s'
 
 errorlog = "-" 
