2ee328e65f140bc2b0b511c2db0f15e3931511c3
Ace_Radom <sichenradomlyu@gmail.com>
2025-10-02 09:54:45 +0200

    fix ip in access log

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@ workers = 2
 forwarded_allow_ips = "*"
 
 accesslog = "-"
-access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s'
+access_log_format = '%(t)s %(p)s %({X-Real-IP}i)s "%(r)s" %(s)s'
 
 errorlog = "-"
