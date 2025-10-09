22affa06604a7552148afc4dae2cd8855450133c
Ace_Radom <sichenradomlyu@gmail.com>
2025-10-09 12:25:00 +0200

    admin page refactor

--- app/static/admin.js
+++ app/static/admin.js
@@ -1,18 +1,39 @@
-function generateInvitationCode() {
-    const username = document.getElementById('invite-username').value;
+const REGEX_USERNAME = /^[A-Z][a-z]*$/;
+
+function validateUsername(username) {
+    return REGEX_USERNAME.test(username);
+}
+
+function generateInvitationCode(username) {
     fetch('/api/invite', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username: username })
+        body: JSON.stringify({ username })
     })
     .then(response => response.json())
     .then(data => {
-        // 处理后端返回的数据
         alert('邀请码: ' + data.code);
     })
     .catch(error => {
         alert('生成失败: ' + error);
     });
+}
+
+if (document.getElementById('adminContainer')) {
+
+    const generateInvitationCodeInput = document.getElementById('generateInvitationCodeInput');
+    const generateInvitationCodeButton = document.getElementById('generateInvitationCodeButton');
+
+    generateInvitationCodeButton.addEventListener('click', function () {
+        let username = generateInvitationCodeInput.value;
+        if (!validateUsername(username)) {
+            alert('用户名不合法："' + username +'"');
+            return;
+        }
+
+        generateInvitationCode(username);
+    });
+
 }
